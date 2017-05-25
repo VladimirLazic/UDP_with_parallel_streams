5ea4c7948aa95257b8b4464015ec6f06a38857e1
interstellaracidtrip <ironmanm72@gmail.com>
2017-05-25 13:48:44 +0200

    Sender done, not tested

--- UDP_with_parallel_streams/sender.c
+++ UDP_with_parallel_streams/sender.c
@@ -5,6 +5,7 @@
 
 #ifdef _WIN32
 #define HAVE_STRUCT_TIMESPEC
+#pragma comment(lib, "Ws2_32.lib")
 #endif // _W
 
 #include<pthread.h>
@@ -39,22 +40,13 @@ typedef struct Datagram
 struct Datagram datagrams[DEFAULT_FILE_LEN];
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
-udp_header *udp_hdr;
-ip_header *ip_hdr;
-ethernet_header *eth_hdr;
-
 
 void packet_handler(unsigned char *param, const struct pcap_pkthdr *packet_header, const unsigned char *packet_data);
 void *device_thread_function(void *params);
-
-void initialize_protocol_headers() {
-
-	//Datagram length inlucdes the size of udp header
-	udp_hdr->datagram_length = sizeof(struct Datagram) + sizeof(udp_header);
-
-	//strcpy(udp_hdr->dest_port, "8080");
-	//strcpy(udp_hdr->src_port, "8080");
-}
+void  create_packet_header(unsigned char *datagram , struct Datagram data);
+unsigned short calculate_ip_checksum(unsigned char *ip_hdr);
+unsigned short BytesTo16(unsigned char X, unsigned char Y);
+unsigned short calculate_udp_checksum(udp_header *udp, ip_header *ip);
 
 void fill_datagrams(char* file_name) {
 	FILE *file;
@@ -90,17 +82,15 @@ void fill_datagrams(char* file_name) {
 
 }
 
-
-
 int main(int argc, char **argv) {
-	pcap_if_t *devices;						// List of network interface controllers
+	pcap_if_t *devices , *selected_devices;						// List of network interface controllers
 	pcap_if_t *device;						// Network interface controller
 	unsigned int netmask;
 	char error_buffer[PCAP_ERRBUF_SIZE];	// Error buffer
 	char filter_exp[] = "udp";
 	struct bpf_program fcode;
 	pthread_t device_thread[NUM_OF_THREADS];
-	int i = 0 , NumberOfThreads = 0;
+	int i = 0 , NumberOfThreads = 0 , device_number[2];
 
 	//Filling datagrams
 	printf("File to be loaded: %s\n", argv[1]);
@@ -126,7 +116,7 @@ int main(int argc, char **argv) {
 		printf("\tDevice: name - %s\n\t        description - %s\n", device->name , device->description);
 	}
 	printf("\n");
-
+	
 	for (device = devices , i = 0; device; device = device->next , i++) {
 		if (pthread_create(&device_thread[i], NULL, &device_thread_function, device)) {
 			printf("Error creating a thread for device: %s\n", device->name);
@@ -148,7 +138,7 @@ void *device_thread_function(void *device) {
 	pcap_if_t *thread_device = (pcap_if_t *)device;
 	pcap_t* device_handle;					// Descriptor of capture device
 	char error_buffer[PCAP_ERRBUF_SIZE];	// Error buffer
-	char packet[12 + sizeof(struct Datagram)];
+	unsigned char packet[sizeof(ethernet_header) + sizeof(ip_header) + sizeof(udp_header) + sizeof(struct Datagram)];
 	int i = 0;
 
 	// Open the capture device
@@ -160,26 +150,18 @@ void *device_thread_function(void *device) {
 		)) == NULL)
 	{
 		printf("\nUnable to open the adapter. %s is not supported by libpcap/WinPcap\n", thread_device->name);
-		return ;
-	}
-
-	//Preparing packets to be sent
-	for (i = 0; i < 6; i++) {
-		//Set destination mac address 1:1:1:1
-		packet[i] = 1;
-	}
-
-	for (i = 6; i < 12; i++) {
-		//Set source mac address 2:2:2:2
-		packet[i] = 2;
-	}
-
+		return;
+	}	
+	
 	
 	for (i = 0; i < NumberOfPackets; i++) {
 		pthread_mutex_lock(&mutex);
 		if (!datagrams[i].sent) {
-			memcpy((packet + 12), &datagrams[i], sizeof(struct Datagram));
+			//Create a udp datagram
+			create_packet_header(packet , datagrams[i]);
+			memcpy((packet + sizeof(ethernet_header) + sizeof(ip_header) + sizeof(udp_header)), &datagrams[i], sizeof(struct Datagram));
 
+			//Send a usp datagram
 			if (pcap_sendpacket(device_handle, packet, 12 + sizeof(struct Datagram)) != 0) {
 				printf("Error sending packet id: %d\n", datagrams[i].datagram_id);
 			} else {
@@ -189,4 +171,141 @@ void *device_thread_function(void *device) {
 		}
 		pthread_mutex_unlock(&mutex);
 	}	
+}
+
+void create_packet_header(unsigned char *datagram , struct Datagram data) {
+	udp_header *udp_hdr = (udp_header *) malloc(sizeof(udp_header));
+	ip_header *ip_hdr = (ip_header *) malloc(sizeof(ip_header));
+	ethernet_header *eth_hdr = (ethernet_header *) malloc(sizeof(ethernet_header));
+	unsigned char ipv4_addr_dst[4];
+	unsigned char eth_addr_src[6];
+	unsigned char ip_helper[sizeof(ip_header) - sizeof(unsigned short)];
+
+
+	// 192.168.1.3 64-5A-04-CD-98-76
+	ipv4_addr_dst[0] = (char)192;
+	ipv4_addr_dst[1] = (char)168;
+	ipv4_addr_dst[2] = (char)1;
+	ipv4_addr_dst[3] = (char)3;
+
+	eth_addr_src[0] = (char)"64";
+	eth_addr_src[1] = (char)"5A";
+	eth_addr_src[2] = (char)"04";
+	eth_addr_src[3] = (char)"CD";
+	eth_addr_src[4] = (char)"98";
+	eth_addr_src[5] = (char)"76";
+
+	//Initializing
+	memset(udp_hdr, 0, sizeof(udp_header));
+	memset(ip_hdr, 0, sizeof(ip_header));
+	memset(eth_hdr, 0, sizeof(ethernet_header));
+
+	//Creating a udp header
+	udp_hdr->src_port = htons(8080);
+	udp_hdr->dest_port = htons(8080);
+	udp_hdr->datagram_length = sizeof(struct Datagram) + sizeof(udp_header);
+
+	//Creating a ip header
+	ip_hdr->next_protocol = 17;		//0x11 UDP protocol
+	memcpy(&(ip_hdr->dst_addr), ipv4_addr_dst, 4 * sizeof(char));
+	memcpy(&(ip_hdr->src_addr), ipv4_addr_dst, 4 * sizeof(char));
+	memcpy(ip_helper, ip_hdr, 22);
+
+	//Creating ip and udp headers
+	udp_hdr->checksum = calculate_udp_checksum(udp_hdr, ip_hdr , data);
+	ip_hdr->checksum = calculate_ip_checksum(ip_helper);
+	
+
+	//Creating a ethernet header
+	memcpy(&(eth_hdr->dest_address), eth_addr_src, 6 * sizeof(unsigned char));	
+	memcpy(&(eth_hdr->src_address), eth_addr_src, 6 * sizeof(unsigned char));
+	eth_hdr->type = htonl(0x0800);
+
+
+	memcpy(datagram, eth_hdr, sizeof(ethernet_header));
+	memcpy(datagram + sizeof(ethernet_header), ip_hdr, sizeof(ip_header));
+	memcpy(datagram + sizeof(ethernet_header) + sizeof(ip_header), udp_hdr, sizeof(udp_header));	
+
+	udp_hdr = NULL;
+	ip_hdr = NULL;
+	eth_hdr = NULL;
+
+	free(udp_hdr);
+	free(ip_hdr);
+	free(eth_hdr);
+}
+
+unsigned short calculate_ip_checksum(unsigned char *ip_hdr) {
+	unsigned short CheckSum = 0;
+	for (int i = 0; i<22; i += 2)
+	{
+		unsigned short Tmp = BytesTo16(ip_hdr[i], ip_hdr[i + 1]);
+		unsigned short Difference = 65535 - CheckSum;
+		CheckSum += Tmp;
+		if (Tmp > Difference) { CheckSum += 1; }
+	}
+	CheckSum = ~CheckSum;
+	return htons(CheckSum);
+}
+
+unsigned short calculate_udp_checksum(udp_header *udp, ip_header *ip , struct Datagram data) {
+	unsigned short CheckSum = 0;
+
+	//length of pseudo_header = Data length + 8 bytes UDP header + Two 4 byte IP's + 1 byte protocol
+	unsigned short pseudo_length = sizeof(struct Datagram) + 8 + 9;
+
+	//If bytes are not an even number, add an extra.
+	pseudo_length += pseudo_length % 2;
+
+	// This is just UDP + Data length.
+	unsigned short length = sizeof(struct Datagram) + 8;  
+
+	//Init
+	unsigned char* pseudo_header = (unsigned char*) malloc(pseudo_length * sizeof(unsigned char));
+	for (int i = 0; i < pseudo_length; i++) {
+		pseudo_header[i] = 0x00; 
+	}
+
+	// Protocol
+	memcpy(pseudo_header, &(ip->next_protocol), 1);
+
+	// Source and Dest IP
+	memcpy(pseudo_header + 1, &(ip->src_addr), 4);
+	memcpy(pseudo_header + 5, &(ip->dst_addr), 4);
+
+	// length is not network byte order yet
+	length = htons(length); 
+
+	//Included twice
+	memcpy(pseudo_header + 9, (void*)&length, 2); 
+	memcpy(pseudo_header + 11, (void*)&length, 2);
+
+	//Source Port
+	memcpy(pseudo_header + 13, &(udp->src_port), 2);
+
+	//Dest Port
+	memcpy(pseudo_header + 15, &(udp->dest_port), 2);
+	memcpy(pseudo_header + 17, &data, sizeof(struct Datagram));
+
+
+	for (int i = 0; i < pseudo_length; i += 2)
+	{
+		unsigned short Tmp = BytesTo16(pseudo_header[i], pseudo_header[i + 1]);
+		unsigned short Difference = 65535 - CheckSum;
+		CheckSum += Tmp;
+		if (Tmp > Difference) { CheckSum += 1; }
+	}
+	CheckSum = ~CheckSum; //One's complement
+
+	pseudo_header = NULL;
+	free(pseudo_header);
+
+	return CheckSum;
+}
+
+unsigned short BytesTo16(unsigned char X, unsigned char Y) {
+	unsigned short Tmp = X;
+	Tmp = Tmp << 8;
+	Tmp = Tmp | Y;
+	return Tmp;
 }
